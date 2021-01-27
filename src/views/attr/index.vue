c61dac7b060c73a65dd5022a397e7026d846da89
shenwenjie <15805199474@163.com>
2021-01-27 14:39:01 +0800

    Update index.vue

--- src/views/attr/index.vue
+++ src/views/attr/index.vue
@@ -1,70 +1,70 @@
 <template>
   <div>
-    <urule :init-data="initData" @success="getSuccess" @error="getError" />
+    <urule :init-data="conditionTree" @success="getSuccess" @error="getError" />
   </div>
 </template>
 
 <script>
-import urule from "@/components/urule/editor/urule/index.vue";
+import urule from '@/components/urule/editor/urule/index.vue';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    urule,
+    urule
   },
   data() {
     return {
       conditionTree: {
-        separator: "and",
+        separator: 'and',
         children: [
           {
-            firstParamField: "swj自选类型参数.Float",
-            secondParamField: "33",
+            firstParamField: 'swj自选类型参数.Float',
+            secondParamField: '33',
             secondParamIsConstant: 1,
-            operation: "LessThen",
+            operation: 'LessThen'
           },
           {
-            separator: "and",
+            separator: 'and',
             children: [
               {
-                firstParamField: "ssssd",
-                secondParamField: "param1y",
-                operation: "GreaterThen",
+                firstParamField: 'ssssd',
+                secondParamField: 'param1y',
+                operation: 'GreaterThen'
               },
               {
                 firstParamField:
-                  "swj自选类型参数.Integer.有一些字段的类型.Integer.有一些字段的类型",
-                operation: "Null",
-              },
-            ],
+                  'swj自选类型参数.Integer.有一些字段的类型.Integer.有一些字段的类型',
+                operation: 'Null'
+              }
+            ]
           },
           {
-            separator: "and",
+            separator: 'and',
             children: [
               {
-                firstParamField: "param1",
-                secondParamField: "23",
+                firstParamField: 'param1',
+                secondParamField: '23',
                 secondParamIsConstant: 1,
-                operation: "GreaterThen",
-              },
-            ],
+                operation: 'GreaterThen'
+              }
+            ]
           },
           {
-            firstParamField: "param1",
-            secondParamField: "param1",
-            operation: "GreaterThen",
-          },
-        ],
-      },
-    };
+            firstParamField: 'param1',
+            secondParamField: 'param1',
+            operation: 'GreaterThen'
+          }
+        ]
+      }
+    }
   },
   methods: {
     getSuccess(data) {
-      console.log(data);
+      console.log(data)
     },
     getError(error) {
-      this.$message.error(error);
-    },
-  },
-};
+      this.$message.error(error)
+    }
+  }
+}
 </script>
